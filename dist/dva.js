9fdc4355560aef29fdf7d9fe5592b554b1d967bb
hmmhmmhm <hmmhmmhm@naver.com>
2019-07-10 03:55:27 +0900

    Remove dist

--- dist/dva.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// First Resolver Test
-var resolver_1 = require("./kor/resolver/resolver");
-resolver_1.Resolver();
--- dist/kor/resolver/event/onGoingEachPlayer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.onGoingEachPlayer = function (value) {
-    return "\n    event\n    {\n        Ongoing - Each Player;\n        " + value.team + ";\n        " + value.player + ";\n    }\n    ";
-};
